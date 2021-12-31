b0217590f738d66f0c5dba5d4e5d0f4763f3591b
Abhishek Kumar <abhishekkr3003@gmail.com>
2021-12-31 23:52:45 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.39%) - LeetHub

---
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int res=0;
+        int i=5;
+        while(i<=n){
+            res+=n/i;
+            i*=5;
+        }
+        return res;
+    }
+};
