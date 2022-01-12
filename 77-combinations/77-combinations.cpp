57f2ef056f3eea5e5040860004c8197274017978
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-01-13 00:21:56 +0530

    Time: 792 ms (5.44%), Space: 164.1 MB (16.76%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    
+    void dfs(int n, int k, int cur, vector<int>ans, vector<vector<int>>&res){
+        if(ans.size()==k) {res.push_back(ans); return;}
+        if(cur>n) return;
+        for(int i=cur;i<=n;i++){
+            ans.push_back(i);
+            dfs(n,k,i+1,ans,res);
+            ans.pop_back();
+        }
+    }
+    
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>cur;
+        vector<vector<int>>res;
+        dfs(n,k,1,cur,res);
+        return res;
+    }
+};
