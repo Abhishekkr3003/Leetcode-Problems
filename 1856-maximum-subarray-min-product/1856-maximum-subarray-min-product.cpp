50a2cb7cee27aaa015b4a739504b0f1ab8ad5ef5
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-02-17 20:24:51 +0530

    Time: 264 ms (59.51%), Space: 98.8 MB (15.18%) - LeetHub

---
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxSumMinProduct(vector<int>& nums) {
+       stack<pair<long long, long long>> st;
+        vector<long long> nsr(nums.size());
+        vector<long long> nsl(nums.size());
+        vector<long long> prefix(nums.size() + 1);
+        prefix[0] = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            while (!st.empty() && st.top().second >= nums[i]) st.pop();
+            if (st.empty())
+                nsl[i] = 0;
+            else
+                nsl[i] = st.top().first + 1;
+            st.push({i,nums[i]});
+        }
+        while (!st.empty()) st.pop();
+        
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top().second >= nums[i]) st.pop();
+            if (st.empty())
+                nsr[i] = nums.size();
+            else
+                nsr[i] = st.top().first;
+            st.push({i,nums[i]});
+        }
+        
+        long long res = 0;
+        for (int i = 0; i < nums.size();i++){
+            // cout<<prefix[nsr[i]]<<" "<<prefix[nsl[i]]<<" "<<nums[i]<<endl;
+            res = max(res,nums[i] * (prefix[nsr[i]] - prefix[nsl[i]]));
+        }
+        int mod=1e9+7;
+        int ans=res%mod;
+        return ans;
+    }
+};
