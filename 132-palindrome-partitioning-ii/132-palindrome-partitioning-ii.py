38ea1704bfa95383451650b7f2409dd6ae02c67f
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-02-24 20:02:36 +0530

    Time: 2584 ms (30.43%), Space: 311.7 MB (10.94%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -1,3 +1,5 @@
+# H | DP - Word Break and Fountain Type | T: O(n^2) | S: O(n^2)
+
 class Solution:
     def minCut(self, s: str) -> int:
         def find_pal(i: int, j: int, t: dict, s: str) -> bool:
@@ -18,8 +20,7 @@ class Solution:
             if res[i]==2002:
                 continue
             for j in range(len(s)-1,i-1,-1):
-                # print(j)
                 if find_pal(i,j,t,s):
                     res[j+1]=min(res[j+1],res[i]+1)
-            # print(res)
+            
         return res[-1]-1
