2306d2248198095a076744ce5bbd3be884a25953
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-02-25 14:53:01 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.32%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+
+public:
+    int compareVersion(string version1, string version2) {
+        int i=0, j=0;
+        int num1=0, num2=0;
+        while(i<version1.length() || j<version2.length()){
+            num1=num2=0;
+            while(i<version1.length() && version1[i]!='.'){
+                num1=num1*10+(version1[i]-'0');
+                i++;
+            }
+            while(j<version2.length() && version2[j]!='.'){
+                num2=num2*10+(version2[j]-'0');
+                j++;
+            }
+            i++;
+            j++;
+            if(num1<num2) return -1;
+            else if(num1>num2) return 1;
+        }
+        return 0;
+    }
+};
