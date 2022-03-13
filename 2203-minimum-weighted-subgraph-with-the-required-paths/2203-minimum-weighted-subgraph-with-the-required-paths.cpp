a29808575a26b4b96cc31918c2ca8815acb58363
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-03-13 12:57:41 +0530

    Time: 1352 ms (7.14%), Space: 169.6 MB (21.43%) - LeetHub

--- 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
+++ 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
@@ -1,11 +1,8 @@
-/* Topic: Graph (Dijkstra) | Difficulty: H | Time: O(nodes+edge*log(nodes)) | Space: O(nodes) */
-
-
 typedef long long ll;
 class Solution {
     vector<ll> dijkstra(int n, vector<pair<int, int>> *graph, int src) {
         vector<ll> distance(n, -1);
-        set<pair<ll,ll>>q;
+        set<pair<ll, ll>> q;
         distance[src] = 0;
         q.insert({0, src});
         while (!q.empty()) {
@@ -17,7 +14,7 @@ class Solution {
                     distance[nbr.first] = dist + nbr.second;
                     q.insert({distance[nbr.first], nbr.first});
                 } else if (distance[nbr.first] > dist + nbr.second) {
-                    q.erase({distance[nbr.first],nbr.first});
+                    q.erase({distance[nbr.first], nbr.first});
                     distance[nbr.first] = dist + nbr.second;
                     q.insert({distance[nbr.first], nbr.first});
                 }
@@ -26,8 +23,9 @@ class Solution {
 
         return distance;
     }
+
 public:
-    long long minimumWeight(int n, vector<vector<int>>& edges, int src1, int src2, int dest) {
+    long long minimumWeight(int n, vector<vector<int>> &edges, int src1, int src2, int dest) {
         vector<pair<int, int>> graph[n];
         vector<pair<int, int>> revGraph[n];
         for (auto edge : edges) {
@@ -39,11 +37,10 @@ public:
         auto destToAll = dijkstra(n, revGraph, dest);
         if (destToAll[src1] == -1 || destToAll[src2] == -1) return -1;
         ll dist = LLONG_MAX;
-        for (int common = 0; common< n; common++) {
-            if(destToAll[common]!=-1 && src1ToAll[common]!=-1 && src2ToAll[common]!=-1)
-                dist= min(dist, destToAll[common] + src1ToAll[common] + src2ToAll[common]);
+        for (int common = 0; common < n; common++) {
+            if (destToAll[common] != -1 && src1ToAll[common] != -1 && src2ToAll[common] != -1)
+                dist = min(dist, destToAll[common] + src1ToAll[common] + src2ToAll[common]);
         }
-      
         return dist;
     }
 };
