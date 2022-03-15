523e542b772eee4e7806a6cc86378c5ef15e7ed0
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-03-16 01:14:47 +0530

    Time: 20 ms (63.44%), Space: 15.2 MB (40.46%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+/* 
+    Time: O(2^n)
+    Space: O(2^n)
+    Tag: DFS
+    Difficulty: E
+*/
+
+class Solution {
+
+    void dfs(vector<vector<int>>&graph, vector<vector<int>>&res, vector<int>curPath, int curNode){
+        curPath.push_back(curNode);
+        if(curNode==graph.size()-1){ res.push_back(curPath); return; }
+        for(int nbr:graph[curNode]){
+            dfs(graph,res,curPath,nbr);
+        }
+    }
+
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>>res;
+        vector<int>curPath;
+        dfs(graph,res,curPath,0);
+        return res;
+    }
+};
