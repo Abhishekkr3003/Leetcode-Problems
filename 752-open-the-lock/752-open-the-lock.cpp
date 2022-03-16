25a96f0fbbe9497d4562710d71682082b649f282
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-03-17 00:54:19 +0530

    Time: 214 ms (75.34%), Space: 30.7 MB (85.26%) - LeetHub

---
+++ 752-open-the-lock/752-open-the-lock.cpp
@@ -0,0 +1,51 @@
+/* 
+    Time: O()
+    Space: O()
+    Tag: 
+    Difficulty: 
+*/
+
+class Solution {
+public:
+    int openLock(vector<string> &deadends, string target) {
+        if (target == "0000") return 0;
+        unordered_set<string> visited;
+        for (string s : deadends) {
+            visited.insert(s);
+        }
+        if(visited.find("0000")!=visited.end()) return -1;
+        queue<string> q;
+        q.push("0000");
+        visited.insert("0000");
+        int steps = 0;
+
+        while (!q.empty()) {
+            steps++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                string cur = q.front();
+                q.pop();
+                string temp = cur;
+                for (int i = 0; i < 4; i++) {
+                    temp = cur;
+                    temp[i] = '0' + ((temp[i] - '0' + 9) % 10);
+                    if (visited.find(temp) == visited.end()) {
+                        if (temp == target) return steps;
+                        visited.insert(temp);
+                        q.push(temp);
+                    }
+                }
+                for (int i = 0; i < 4; i++) {
+                    temp = cur;
+                    temp[i] = '0' + ((temp[i] - '0' + 1) % 10);
+                    if (visited.find(temp) == visited.end()) {
+                        if (temp == target) return steps;
+                        visited.insert(temp);
+                        q.push(temp);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
