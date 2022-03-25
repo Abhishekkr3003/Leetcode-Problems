b35554d2be95018ae7095be4b068ad52d3224030
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-03-25 09:41:06 +0530

    Time: 68 ms (5.79%), Space: 92.6 MB (7.87%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,28 @@
+/*
+    Time: O()
+    Space: O()
+    Tag:
+    Difficulty:
+*/
+
+int findMinCost(vector < vector<vector<int>>>& t, int index, int m, int n, vector<vector<int>>& costs) {
+    if (index == costs.size()) return 0;
+    if (t[index][m][n] != -1) return t[index][m][n];
+    int minCost = INT_MAX;
+    if (m < costs.size() / 2) {
+        minCost = min(minCost, findMinCost(t, index + 1, m + 1, n, costs) + costs[index][0]);
+    }
+    if (n < costs.size() / 2) {
+        minCost = min(minCost, findMinCost(t, index + 1, m, n + 1, costs) + costs[index][1]);
+    }
+    return t[index][m][n] = minCost;
+}
+
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        vector < vector<vector<int>>>t(n + 1, vector<vector<int>>(n + 1, vector<int>(n, -1)));
+        return findMinCost(t, 0, 0, 0, costs);
+    }
+};
