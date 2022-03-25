5e92cc92f24b83da192dca84aed20da2955b38c2
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-03-25 11:26:24 +0530

    Time: 0 ms (100.00%), Space: 8 MB (47.20%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,28 +1,26 @@
 /*
-    Time: O(n^3)
-    Space: O(n^3)
-    Tag: DP (01 Knapsack)
+    Time: O(nlogn)
+    Space: O(n)
+    Tag: Greedy, Sorting
     Difficulty: M
 */
-
-int findMinCost(vector < vector<vector<int>>>& t, int index, int m, int n, vector<vector<int>>& costs) {
-    if (index == costs.size()) return 0;
-    if (t[index][m][n] != -1) return t[index][m][n];
-    int minCost = INT_MAX;
-    if (m < costs.size() / 2) {
-        minCost = min(minCost, findMinCost(t, index + 1, m + 1, n, costs) + costs[index][0]);
-    }
-    if (n < costs.size() / 2) {
-        minCost = min(minCost, findMinCost(t, index + 1, m, n + 1, costs) + costs[index][1]);
-    }
-    return t[index][m][n] = minCost;
-}
 
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n = costs.size();
-        vector < vector<vector<int>>>t(n + 1, vector<vector<int>>(n + 1, vector<int>(n, -1)));
-        return findMinCost(t, 0, 0, 0, costs);
+        int sum = 0;
+        for (int i = 0; i < costs.size(); i++) {
+            sum += costs[i][1];
+        }
+        vector<int> temp;
+        for (int i = 0; i < costs.size(); i++) {
+            temp.push_back(costs[i][0] - costs[i][1]);
+        }
+        sort(temp.begin(), temp.end());
+        int sum2 = 0;
+        for (int i = 0; i < costs.size() / 2; i++) {
+            sum2 += temp[i];
+        }
+        return sum + sum2;
     }
 };
