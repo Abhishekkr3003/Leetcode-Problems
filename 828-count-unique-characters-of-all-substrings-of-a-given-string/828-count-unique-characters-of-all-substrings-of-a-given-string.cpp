c370c0a6034f3f8dd17e6a7435260aaf364215f0
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-03-27 00:41:47 +0530

    Time: 93 ms (31.83%), Space: 11.6 MB (54.96%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -12,14 +12,9 @@ class Solution {
         if (s.length() == 1) return 1;
         vector<vector<int>> subtract(26, { 0,0,-1,0 });
         int f = 0, sec = 0;
-        // cout<<"hrer";
         long long toBeSub=0;
         for (int i = 0;i <s.length();i++) {
             sec += f;
-            
-            // cout<<"here\n";
-            // cout<<i<<" "<<s[i]<<"\n";
-            // cout<<subtract[s[i] - 'A'][0]<<" "<<subtract[s[i] - 'A'][1]<<" "<<subtract[s[i] - 'A'][2]<<" "<<subtract[s[i] - 'A'][3]<<endl;
             sec += ((long long)(i + 1) * (i + 2)) / 2ll;
             if (subtract[s[i] - 'A'][2] != -1) {
                 toBeSub += (2 * subtract[s[i] - 'A'][1]);
@@ -32,9 +27,7 @@ class Solution {
                 subtract[s[i] - 'A'][2] = i;
                 subtract[s[i] - 'A'][1] = i+1;
             }
-            // cout<<subtract[s[i] - 'A'][0]<<" "<<subtract[s[i] - 'A'][1]<<" "<<subtract[s[i] - 'A'][2]<<" "<<subtract[s[i] - 'A'][3]<<endl;
             sec -= toBeSub;
-            // cout<<"TEmp: "<<toBeSub<<endl;
             f = sec;
             sec = 0;
             
