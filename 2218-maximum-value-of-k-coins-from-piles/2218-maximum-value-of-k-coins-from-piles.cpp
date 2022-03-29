7c15b59a44998ffd8fde087f3043e59cfd7cb467
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-03-29 22:42:13 +0530

    Time: 365 ms (63.45%), Space: 18.6 MB (51.76%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,32 +1,21 @@
 /*
-    Time: O()
-    Space: O()
-    Tag:
-    Difficulty:
+    Time: O(sum(piles[i].length)*k)
+    Space: O(len(piles)*k)
+    Tag: DP (Subsets Recursion Memoization) (Kind of Knapsack)
+    Difficulty: H
 */
 
 
 class Solution {
     vector<vector<int>>piles;
     int solve(vector<vector<int>>& t, int pileNo, int k) {
-        // cout<<pileNo<<" "<<k<<endl;
-        if (k == 0) {
-            // cout<<"returning zero";
-            return 0;
-        }
+        if (k == 0) return 0;
         if (pileNo >= piles.size()) return INT_MIN;
-        // cout<<"here\n";
         if (t[pileNo][k] != -1) return t[pileNo][k];
         int res = solve(t, pileNo + 1, k);
-        // cout<<"There "<<pileNo<<endl;
         for (int i = 0; i < min((int)(piles[pileNo].size()), k); i++) {
-            // cout<<"Loop: "<<i<<endl;
             res = max(res, piles[pileNo][i] + solve(t, pileNo + 1, k - i - 1));
-            // cout<<i<<" res "<<res<<endl;
-            // cout<<"here2\n";
         }
-        // cout<<"here3\n";
-        // cout<<"Before Return "<<pileNo<<" "<<k<<endl;
         return t[pileNo][k] = res;
     }
 
