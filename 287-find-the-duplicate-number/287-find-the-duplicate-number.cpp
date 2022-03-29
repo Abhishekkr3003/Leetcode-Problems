e0cb594505c48100441b94cf61ddb734dd9f915c
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-03-29 10:26:20 +0530

    Time: 283 ms (13.75%), Space: 61.3 MB (61.36%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -3,22 +3,17 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         vector<int>bitCount(32,0);
-        for(int i=1;i<nums.size();i++){
-            for(int j=0;j<32;j++){
-                bitCount[j]-=((i>>j)&1);
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<32;j++) {
+                if(nums[i]&(1<<j)) bitCount[j]++;
+            }
+            for(int j=0;j<32;j++) {
+                if(i&(1<<j)) bitCount[j]--;
             }
         }
-        for(int num:nums){
-            for(int j=0;j<32;j++){
-                bitCount[j]+=((num>>j)&1);
-            }
-        }
-        // for(int i=0;i<32;i++) cout<<
         int res=0;
         for(int i=0;i<32;i++){
-            if(bitCount[i]>0){
-                res+=(1<<i);
-            }
+            if(bitCount[i]>0) res+=(1<<i);
         }
         return res;
     }
