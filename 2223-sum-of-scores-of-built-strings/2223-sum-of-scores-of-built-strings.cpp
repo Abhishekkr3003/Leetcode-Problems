152737e4a30a601ed03837c59dfc6767338c6238
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-03 00:24:52 +0530

    Time: 63 ms (91.67%), Space: 20.6 MB (91.67%) - LeetHub

---
+++ 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.cpp
@@ -0,0 +1,30 @@
+typedef long long ll;
+
+class Solution {
+public:
+    long long sumScores(string s) {
+        ll res=s.length();
+        vector<int>t(s.length());
+        t[0]=0;
+        int left=0, right=0;
+        for(int i=1;i<s.length();i++){
+            if(i>right){
+                left=right=i;
+                while(right<s.length() && s[right]==s[right-left]) right++;
+                t[i]=right-left;
+                right--;
+            }else{
+                if(t[i-left]+i<=right){
+                    t[i]=t[i-left];
+                }else{
+                    left=i;
+                    while(right<s.length() && s[right]==s[right-left]) right++;
+                    t[i]=right-left;
+                    right--;
+                }
+            }
+            res+=t[i];
+        }
+        return res;
+    }
+};
