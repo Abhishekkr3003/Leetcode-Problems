ad347cc7079c242320728fe4e7f6484ad4dc85bd
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-02 17:03:43 +0530

    Time: 145 ms (82.28%), Space: 56.3 MB (41.75%) - LeetHub

---
+++ 891-sum-of-subsequence-widths/891-sum-of-subsequence-widths.cpp
@@ -0,0 +1,27 @@
+typedef long long ll;
+
+/*
+    Time: O()
+    Space: O()
+    Tag:
+    Difficulty:
+*/
+
+class Solution {
+public:
+    int sumSubseqWidths(vector<int>& nums) {
+        int mod = 1e9 + 7;
+        vector<int>pow2(nums.size());
+        pow2[0] = 1;
+        for (int i = 1;i < nums.size();i++) {
+            pow2[i] = (pow2[i - 1] * 2ll) % mod;
+        }
+        int res = 0;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 0;i < nums.size();i++) {
+            res = (res + ((ll)nums[i] * (pow2[i] - pow2[n - 1 - i]))) % mod;
+        }
+        return res;
+    }
+};
