3e0f2c8aeff11f5fa3ee2b6a727a82a1a78fbb70
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-03 16:21:37 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (96.80%) - LeetHub

---
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string tictactoe(vector<vector<int>>& moves) {
+        vector<int>brd1(9,false), brd2(9,false);
+        for(int i=0;i<moves.size();i++){
+            if((i&1)==0) brd1[moves[i][0]*3+moves[i][1]]=true;
+            else brd2[moves[i][0]*3+moves[i][1]]=true;
+        }
+        if((brd1[0]+brd1[3]+brd1[6]==3) || (brd1[1]+brd1[4]+brd1[7]==3) || (brd1[2]+brd1[5]+brd1[8]==3) || (brd1[0]+brd1[1]+brd1[2]==3) || (brd1[3]+brd1[4]+brd1[5]==3) || (brd1[6]+brd1[7]+brd1[8]==3) || (brd1[0]+brd1[4]+brd1[8]==3) || (brd1[2]+brd1[4]+brd1[6]==3)) return "A";
+        if((brd2[0]+brd2[3]+brd2[6]==3) || (brd2[1]+brd2[4]+brd2[7]==3) || (brd2[2]+brd2[5]+brd2[8]==3) || (brd2[0]+brd2[1]+brd2[2]==3) || (brd2[3]+brd2[4]+brd2[5]==3) || (brd2[6]+brd2[7]+brd2[8]==3) || (brd2[0]+brd2[4]+brd2[8]==3) || (brd2[2]+brd2[4]+brd2[6]==3)) return "B";
+        if(moves.size()==9) return "Draw";
+        return "Pending";
+    }
+};
