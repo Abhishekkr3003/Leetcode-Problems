9515e7bee3f5ab1784c7340819cd63e5090dd5e4
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-06 01:15:43 +0530

    Time: 537 ms (23.32%), Space: 69.7 MB (41.03%) - LeetHub

---
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -0,0 +1,26 @@
+/*
+    Time: O()
+    Space: O()
+    Tag:
+    Difficulty:
+*/
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int>t;
+        t.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]) t.push_back(nums[i]);
+        }
+        int res = nums.size() - 1;
+        int restNum = nums.size() - 1;
+        for (int i = 0; i < nums.size(); i++) {
+            int range = nums[i] + restNum;
+            int inRange = restNum - (upper_bound(t.begin(), t.end(), range)-upper_bound(t.begin(), t.end(), nums[i]));
+            res = min(res, inRange);
+        }
+        return res;
+    }
+};
