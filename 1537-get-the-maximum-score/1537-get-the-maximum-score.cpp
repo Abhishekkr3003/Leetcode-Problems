b4e25fb40b71f054959f8779eddb7142add96306
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-17 00:26:11 +0530

    Time: 156 ms (51.21%), Space: 60.3 MB (28.18%) - LeetHub

---
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums1, vector<int>& nums2) {
+        vector<long long>prefNums1(nums1.size() + 1), prefNums2(nums2.size() + 1);
+        int mod=1e9+7;
+        prefNums1[0] = 0, prefNums2[0] = 0;
+        for (int i = 1; i < prefNums1.size(); i++) {
+            prefNums1[i] =(prefNums1[i]+ nums1[i - 1] + prefNums1[i - 1]);
+        }
+        for (int i = 1; i < prefNums2.size(); i++) {
+            prefNums2[i] =( prefNums2[i]+nums2[i - 1] + prefNums2[i - 1]);
+        }
+        int res = 0;
+        int lastIdx1 = 0, lastIdx2 = 0;
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            int idx = lower_bound(nums2.begin(), nums2.end(), nums1[i]) - nums2.begin();
+            if (idx >= nums2.size() || nums2[idx] != nums1[i]) continue;
+            res =(res+ max(prefNums1[i + 1] - prefNums1[lastIdx1], prefNums2[idx + 1] - prefNums2[lastIdx2]))%mod;
+            // cout<<idx<<endl;
+            // cout<<res<<endl;
+            lastIdx1 = i + 1;
+            lastIdx2 = idx + 1;
+        }
+        res =(res+ max(prefNums1.back() - prefNums1[lastIdx1], prefNums2.back() - prefNums2[lastIdx2]))%mod;
+        return res;
+    }
+};
