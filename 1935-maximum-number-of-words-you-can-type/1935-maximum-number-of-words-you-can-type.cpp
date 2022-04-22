5d214bfd88542fd2456937267d49251f54028139
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-23 01:56:15 +0530

    Time: 4 ms (63.40%), Space: 6.6 MB (63.40%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,28 @@
+/* 
+    Time: O()
+    Space: O()
+    Tag: 
+    Difficulty: 
+*/
+
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        int res = 0;
+        bool broken[26] = {0};
+        for (char &ch : brokenLetters) broken[ch - 'a'] = true;
+        stringstream parser(text);
+        string temp;
+        while (getline(parser, temp, ' ')) {
+            bool count = true;
+            for (char &ch : temp) {
+                if (broken[ch - 'a']) {
+                    count = false;
+                    break;
+                }
+            }
+            if (count) res++;
+        }
+        return res;
+    }
+};
