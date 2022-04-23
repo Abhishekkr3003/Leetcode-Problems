336ea5dec2e0140b6df5eaabbdde53f19c2e9629
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-23 17:53:09 +0530

    Time: 390 ms (65.08%), Space: 98.3 MB (99.70%) - LeetHub

---
+++ 2111-minimum-operations-to-make-the-array-k-increasing/2111-minimum-operations-to-make-the-array-k-increasing.cpp
@@ -0,0 +1,29 @@
+/* 
+    Time: O()
+    Space: O()
+    Tag: 
+    Difficulty: 
+*/
+
+class Solution {
+public:
+    int kIncreasing(vector<int> &arr, int k) {
+        int res = 0;
+        vector<int> lis;
+
+        for (int i = 0; i < k; i++) {
+            int size = 0;
+            for (int j = i; j < arr.size(); j += k) {
+                size++;
+                auto it = upper_bound(lis.begin(), lis.end(), arr[j]);
+                if (it == lis.end())
+                    lis.push_back(arr[j]);
+                else
+                    *it = arr[j];
+            }
+            res += size - lis.size();
+            lis.clear();
+        }
+        return res;
+    }
+};
