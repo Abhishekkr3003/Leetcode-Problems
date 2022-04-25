1f38d4cf4bc2f8688dd8666b5137dd9a4a1f8397
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-25 16:27:55 +0530

    Time: 161 ms (69.70%), Space: 89.8 MB (59.37%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -0,0 +1,36 @@
+/* 
+    Time: O()
+    Space: O()
+    Tag: 
+    Difficulty: 
+*/
+
+class Solution {
+public:
+    vector<int> goodDaysToRobBank(vector<int> &security, int time) {
+        int n = security.size();
+        vector<int> leftSide(n), righSide(n);
+        leftSide[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (security[i - 1] >= security[i])
+                leftSide[i] = leftSide[i - 1];
+            else
+                leftSide[i] = i;
+        }
+        righSide[n - 1] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (security[i + 1] < security[i])
+                righSide[i] = i;
+            else
+                righSide[i] = righSide[i + 1];
+        }
+        // for (int num : leftSide) cout << num << " ";
+        // cout << endl;
+        // for (int num : righSide) cout << num << " ";
+        vector<int> res;
+        for (int i = 0; i < n; i++) {
+            if (leftSide[i] <= i - time && righSide[i] >= i + time) res.push_back(i);
+        }
+        return res;
+    }
+};
