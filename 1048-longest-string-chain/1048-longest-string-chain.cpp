9e2e075d6e7dcf3868e9d4848b79d6d5c38b6d4e
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-04-26 19:14:56 +0530

    Time: 167 ms (35.39%), Space: 16.5 MB (73.66%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,40 @@
+/* 
+    Time: O()
+    Space: O()
+    Tag: 
+    Difficulty: 
+*/
+
+class Solution {
+public:
+    int longestStrChain(vector<string> &words) {
+        unordered_map<int, vector<pair<string, int>>> mp;
+        for (string &s : words) {
+            mp[s.length()].push_back({s, 1});
+        }
+        int res = 1;
+        for (int i = 2; i <= 16; i++) {
+            if (mp.find(i) == mp.end() || mp.find(i - 1) == mp.end()) continue;
+            for (int j = 0; j < mp[i].size(); j++) {
+                for (int k = 0; k < mp[i - 1].size(); k++) {
+                    int ptr1 = 0, ptr2 = 0;
+                    while (ptr2 < i - 1 && mp[i][j].first[ptr1] == mp[i - 1][k].first[ptr2]) {
+                        ptr1++;
+                        ptr2++;
+                    }
+                    ptr1++;
+                    while (ptr2 < i - 1 && mp[i][j].first[ptr1] == mp[i - 1][k].first[ptr2]) {
+                        ptr1++;
+                        ptr2++;
+                    }
+                    if (ptr2 == i - 1) {
+                        // cout<<mp[i][j].first<<" "<<mp[i - 1][k].first<<endl;
+                        mp[i][j].second = max(mp[i][j].second,mp[i - 1][k].second + 1);
+                        res = max(res, mp[i][j].second);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
