94aaa44043691ebf22d686236dc48e1c298527d5
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-05-23 00:20:51 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (41.72%) - LeetHub

---
+++ 274-h-index/274-h-index.cpp
@@ -0,0 +1,18 @@
+/* 
+    Time: O()
+    Space: O()
+    Tag: 
+    Difficulty: 
+*/
+
+class Solution {
+public:
+    int hIndex(vector<int> &citations) {
+        sort(citations.rbegin(), citations.rend());
+        for (int i = 0; i < citations.size() - 1; i++) {
+            if (i + 1 <= citations[i] && citations[i + 1] <= i + 1) return i + 1;
+        }
+        if (citations.size() <= citations.back()) return citations.size();
+        return 0;
+    }
+};
