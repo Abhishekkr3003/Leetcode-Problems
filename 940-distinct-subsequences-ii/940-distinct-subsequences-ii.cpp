28a0acca3ad33b8794ab1eeefb34785755177ebf
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-05-26 20:48:19 +0530

    Time: 11 ms (34.98%), Space: 6.5 MB (90.85%) - LeetHub

---
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        vector<int> t(26, 0);
+        int mod=1e9+7;
+        long long sum = 0;
+        for (char &ch : s) {
+            sum = 0;
+            for (int i = 0; i < 26; i++) {
+                sum = (sum+ t[i])%mod;
+            }
+            t[ch - 'a'] = (sum + 1)%mod;
+        }
+        sum = 0;
+        for (int i = 0; i < 26; i++) {
+            sum = (sum+ t[i])%mod;
+        }
+        return sum;
+    }
+};
