b986d6464ca4025de0e6ba067af4da6f904247fe
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-05-29 07:51:12 +0530

    Time: 72 ms (56.30%), Space: 15.7 MB (64.52%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+class Solution {
+public:
+    int maxProduct(vector<string> &words) {
+        vector<int> freq(words.size(), 0);
+        for (int i = 0; i < words.size(); i++) {
+            for (char ch : words[i]) {
+                freq[i] = freq[i] | (1 << (ch - 'a'));
+            }
+        }
+        // for(int num:freq) cout<<num<<endl;
+        int res = 0;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = i + 1; j < words.size(); j++) {
+                if ((freq[i] & freq[j]) == 0) res = max(res, (int)(words[i].length() * words[j].length()));
+            }
+        }
+        return res;
+    }
+};
