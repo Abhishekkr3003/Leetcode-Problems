e750a3452ebdabca55bf0a6e7d752469a8b2a7b7
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-05-31 01:17:19 +0530

    Time: 176 ms (21.15%), Space: 13.5 MB (17.31%) - LeetHub

---
+++ 1505-minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits/1505-minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.cpp
@@ -0,0 +1,91 @@
+/* 
+    Time: O()
+    Space: O()
+    Tag: 
+    Difficulty: 
+*/
+
+class SegmentTree {
+    vector<int>tree;
+
+public:
+    SegmentTree(int size) {
+        tree.resize(4*size+1,0);
+    }
+    
+    void printTree(){
+        for(int num:tree) cout<<num<<"";
+        cout<<endl;
+    }
+
+    void updateTree(int lo, int hi, int index, int upd) {
+        if (upd < lo || upd > hi) return;
+        if (lo == hi) {
+            // cout<<"lo: "<<lo<<endl;
+            tree[index]++;
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        updateTree(lo, mid, 2 * index, upd);
+        updateTree(mid + 1, hi, 2 * index + 1, upd);
+        tree[index] = tree[2 * index] + tree[2 * index + 1];
+    }
+
+    int queryTree(int lo, int hi, int index, int qs, int qe) {
+        if (qe < lo || qs > hi) return 0;
+        if (qe >= hi && qs <= lo) return tree[index];
+
+        int mid = lo + (hi - lo) / 2;
+
+        int left = queryTree(lo, mid, 2 * index, qs, qe);
+        int right = queryTree(mid + 1, hi, 2 * index + 1, qs, qe);
+        return left + right;
+    }
+};
+
+class Solution {
+public:
+    string minInteger(string num, int k) {
+        queue<int> pos[10];
+        for (int i = 0; i < num.length(); i++) {
+            pos[num[i] - '0'].push(i);
+        }
+        string res = "";
+        SegmentTree *seg = new SegmentTree((int)num.length());
+        for (int i = 0; i < num.length(); i++) {
+            // cout<<i<<endl;
+            if (num[i] == '-') continue;
+            int digit = num[i] - '0';
+            
+            bool swapped = false;
+            for (int j = 0; j < digit; j++) {
+                if (pos[j].size() > 0) {
+                    // cout<<j<<endl;
+                    int curNumIndex = pos[j].front();
+                    int shifts = seg->queryTree(0, num.length() - 1, 1, i, pos[j].front());
+                    // cout<<"Here "<<i<<" "<<shifts<<" "<<curNumIndex<<endl;
+                    // seg->printTree();
+                    if (curNumIndex - i - shifts <= k) {
+                        
+                        int shiftIndex = pos[j].front();
+                        // cout<<"shiftIndex: "<<shiftIndex<<endl;
+                        seg->updateTree(0, num.length() - 1, 1, shiftIndex);
+                        // seg->printTree();
+                        k -= shiftIndex - i-shifts;
+                        pos[j].pop();
+                        res += num[shiftIndex];
+                        num[shiftIndex] = '-';
+                        swapped = true;
+                        i--;
+                        break;
+                    }
+                }
+            }
+            if (!swapped) {
+                res += num[i];
+                pos[digit].pop();
+            }
+        }
+        return res;
+    }
+};
