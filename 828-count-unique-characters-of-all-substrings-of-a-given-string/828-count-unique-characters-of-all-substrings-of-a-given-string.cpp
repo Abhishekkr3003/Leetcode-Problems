e46c384ecabc4d7d1557b20585df75e0c3a57c30
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-01 01:33:26 +0530

    Time: 44 ms (84.24%), Space: 11.3 MB (84.30%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,37 +1,22 @@
-/*
-    Time: O(n)
-    Space: O(1)
-    Tag: DP (Linear DP on String)
-    Difficulty: VH
-    Notes: https://drive.google.com/drive/u/0/folders/1Vtkt7oEvnr_P6ZM9BrgpNt0-4ASWJhzc
-*/
-
 class Solution {
-    public:
+public:
     int uniqueLetterString(string s) {
-        if (s.length() == 1) return 1;
-        vector<vector<int>> subtract(26, { 0,0,-1,0 });
-        int f = 0, sec = 0;
-        long long toBeSub=0;
-        for (int i = 0;i <s.length();i++) {
-            sec += f;
-            sec += ((long long)(i + 1) * (i + 2)) / 2ll;
-            if (subtract[s[i] - 'A'][2] != -1) {
-                toBeSub += (2 * subtract[s[i] - 'A'][1]);
-                toBeSub += subtract[s[i] - 'A'][3];
-                subtract[s[i] - 'A'][3]+=subtract[s[i] - 'A'][1];
-                subtract[s[i] - 'A'][0] = toBeSub;
-                subtract[s[i] - 'A'][1] = i - subtract[s[i] - 'A'][2];
-                subtract[s[i] - 'A'][2] = i;
-            } else {
-                subtract[s[i] - 'A'][2] = i;
-                subtract[s[i] - 'A'][1] = i+1;
-            }
-            sec -= toBeSub;
-            f = sec;
-            sec = 0;
+        int prev=0,cur=0;
+        vector<pair<int,int>>t(26,{-1,-1});
+        int res=0;
+        
+        for(int i=0;i<s.length();i++){
+            cur=0;
             
+            cur=prev-(t[s[i]-'A'].first-t[s[i]-'A'].second);
+            cur+=(i-t[s[i]-'A'].first);
+            
+            t[s[i]-'A'].second=t[s[i]-'A'].first;
+            t[s[i]-'A'].first=i;       
+        
+            res+=cur; 
+            prev=cur;
         }
-        return f;
+        return res;
     }
 };
