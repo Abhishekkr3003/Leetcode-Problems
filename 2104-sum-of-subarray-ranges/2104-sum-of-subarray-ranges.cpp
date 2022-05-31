b613c609a84dde5bb36b328a9b00764c1797649b
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-05-31 23:38:48 +0530

    Time: 60 ms (59.51%), Space: 14.8 MB (5.02%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,29 +1,29 @@
-/*
-    Time: O()
-    Space: O()
-    Tag:
-    Difficulty:
-*/
-
 typedef long long ll;
-
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        stack<vector<ll>>NSL; //{Prev_Smallest_Val, res_till_prev_val, prev_val_index}
-        stack<vector<ll>>NGL; //{Prev_Largest_Val, res_till_prev_val, prev_val_index}
-        NSL.push({ INT_MIN, 0, -1 });
-        NGL.push({ INT_MAX,0,-1 });
-        ll res = 0;
-        ll nextNSLval = 0, nextNGLval = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            while (nums[i] <= NSL.top()[0]) NSL.pop();
-            while (nums[i] >= NGL.top()[0]) NGL.pop();
-            nextNGLval = nums[i] * (i - NGL.top()[2]) + NGL.top()[1];
-            nextNSLval = nums[i] * (i - NSL.top()[2]) + NSL.top()[1];
-            res += nextNGLval - nextNSLval;
-            NGL.push({ nums[i],nextNGLval,i });
-            NSL.push({ nums[i],nextNSLval,i });
+        ll res=0;
+        stack<vector<ll>>ns, ng;
+        
+        for(ll i=0;i<nums.size();i++){
+            
+            while(!ns.empty() && ns.top()[0]>nums[i]) ns.pop();
+            while(!ng.empty() && ng.top()[0]<nums[i]) ng.pop();
+            
+            if(ns.empty()){
+                ns.push({nums[i],i,nums[i]*(i+1)});
+            }else{
+                ns.push({nums[i],i,nums[i]*(i-ns.top()[1])+ns.top()[2]});
+            }
+            if(ng.empty()){
+                ng.push({nums[i],i,nums[i]*(i+1)});
+            }else{
+                ng.push({nums[i],i,nums[i]*(i-ng.top()[1])+ng.top()[2]});
+            }
+            
+            res-=ns.top()[2];
+            res+=ng.top()[2];
+            
         }
         return res;
     }
