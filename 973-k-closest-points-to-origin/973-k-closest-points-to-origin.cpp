60143469848ef12d99202cfd15c0d4b1fef5c614
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-02 02:08:11 +0530

    Time: 2184 ms (5.00%), Space: 57.1 MB (86.06%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,13 +1,18 @@
+/*
+    Time: O(n)  Note: QuickSelect N + N/2 + N/4 + N/8+ ... + N/N === 2N
+    Space: O(1) Except for size k for giving the result
+    Tag: QuickSelect
+    Difficulty: M
+*/
+
 class Solution {
     int getDist(vector<int>&pt){
         return pt[0]*pt[0]+pt[1]*pt[1];
     }
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>>res(k);
         int lo=0, hi=points.size()-1;
         while(true){
-            // cout<<lo<<" "<<hi<<" "<<k<<endl;
             int i=lo-1;
             int pivot=getDist(points[hi]);
             for(int j=lo;j<hi;j++){
@@ -17,10 +22,6 @@ public:
             }
             swap(points[++i],points[hi]);
             if(i+1==k) {
-                // cout<<"/here";
-                for(int j=0;j<=i;j++){
-                    res[j]=points[j];
-                }
                 break;
             }
             else if(i+1>k) {
@@ -29,6 +30,6 @@ public:
                 lo=i+1;
             }
         }
-        return res;
+        return vector<vector<int>>(points.begin(), points.begin() + k);;
     }
 };
