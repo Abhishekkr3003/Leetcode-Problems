42d75e118b30565f94d3afb8574ad2fcca9da76c
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-05 01:18:33 +0530

    Time: 23 ms (41.80%), Space: 5.9 MB (32.91%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0) return false;
-        vector<int>t;
-        while(x){
-            t.push_back(x%10);
+        if(x==0) return true;
+        if(x%10==0) return false;
+        int temp=0;
+        while(x>temp){
+            temp=(temp*10)+(x%10);
             x/=10;
         }
-        int i=0, j=t.size()-1;
-        while(i<j){
-            if(t[i++]!=t[j--]) return false;
-        }
-        return true;
+        // cout<<x<<" "<<temp;
+        return (x==temp) || (x==temp/10);
     }
 };
