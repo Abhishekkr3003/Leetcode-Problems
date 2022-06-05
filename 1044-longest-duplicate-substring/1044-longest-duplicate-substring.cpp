ee7abe9796c13f1a1dce51a24fc80db897022527
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-05 17:57:56 +0530

    Time: 1935 ms (10.77%), Space: 405.9 MB (24.35%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
@@ -1,50 +1,51 @@
-typedef long long ll;
-
 class Solution {
-    int possible(string &s, int len){
-        ll p=26, mod=1e9+7;
-        ll power=1;
-        ll hash=0;
-        int i=s.length()-len;
-        for(;i<s.length()-1;i++){
-            hash=(hash+(s[i]-'a'+1)*power)%mod;
-            power=(power*p)%mod;
-        }
-        hash=(hash+(s[i]-'a'+1)*power)%mod;
+    string ans="";
+    
+    bool check(string &s, int len){
         unordered_map<int,vector<int>>mp;
+        long long hash=0;
+        int mod=1e9+7;
+        int pm=26;
+        long long p=1;
+        for(int i=s.length()-len;i<s.length();i++){
+            hash=(hash+(s[i]-'a'+1)*p)%mod;
+            if(i==s.length()-1) break;
+            p=(p*pm)%mod;
+        }
         mp[hash].push_back(s.length()-len);
-        for(i=s.length()-len-1;i>=0;i--){
-            hash=(mod+hash-((s[i+len]-'a'+1)*power)%mod)%mod;
-            hash=(hash*p)%mod;
+        for(int i=s.length()-len-1;i>=0;i--){
+            hash=(hash-((s[i+len]-'a'+1)*p)%mod+mod)%mod;
+            hash=((hash%mod)*(pm%mod))%mod;
             hash=(hash+(s[i]-'a'+1))%mod;
-            if(mp.find(hash)!=mp.end()) {
-                string tmp=s.substr(i,len);
-                for(int start:mp[hash]){
-                    if(s.substr(start,len)==tmp) return start;
+            auto it=mp.find(hash);
+            if(it!=mp.end()){
+                string cur=s.substr(i,len);
+                for(int &num: it->second){
+                    if(s.substr(num,len)==cur){
+                        ans=cur;
+                        return true;
+                    }
                 }
+                
             }
             mp[hash].push_back(i);
         }
-        return -1;
+        return false;
     }
     
 public:
     string longestDupSubstring(string s) {
         int lo=0, hi=s.length();
         int mid=0;
-        string res="";
-        int start=-1;
-        int len=-1;
+        
+        
         while(lo<=hi){
             mid=lo+(hi-lo)/2;
-            // if(mid==0) return possible(s,1);
-            int temp=possible(s,mid);
-            if(temp!=-1) {
-                start=temp;
-                len=mid;
-                lo=mid+1;
-            }else hi=mid-1;
+            
+            if(check(s,mid)) lo=mid+1;
+            else hi=mid-1;
+            
         }
-        return len==-1?"":s.substr(start,len);
+        return ans;
     }
 };
