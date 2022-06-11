3ce9d11ceb56a7ccb3d634f0b8973228f54fab9f
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-11 23:14:12 +0530

    Time: 10 ms (45.31%), Space: 11.3 MB (89.64%) - LeetHub

--- 330-patching-array/330-patching-array.cpp
+++ 330-patching-array/330-patching-array.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    int minPatches(vector<int> &nums, int n) {
-        int res = 0;
-        long long curMax = 1;
-        int idx = 0;
-        while (curMax <= n) {
-            if (idx<nums.size() && curMax >= nums[idx])
-                curMax = curMax + nums[idx++];
-            else {
-                res++;
-                curMax += curMax;
+    int minPatches(vector<int>& nums, int n) {
+        long long reachable=0;
+        int ptr=0;
+        int extra=0;
+        while(reachable<n){
+            // cout<<reachable<<" "<<ptr<<endl;
+            while(ptr<nums.size() && nums[ptr]<=reachable+1){
+                
+                reachable=max(reachable,nums[ptr++]+reachable);
+                // cout<<"Val: "<<reachable<<" "<<ptr<<endl;
+            }
+            // cout<<"here";
+            // cout<<reachable<<" "<<ptr<<endl;
+            if(reachable<n && (ptr>=nums.size() || reachable<nums[ptr]-1)){
+                extra++;
+                reachable+=(reachable+1);
             }
         }
-        return res;
+        return extra;
     }
 };
