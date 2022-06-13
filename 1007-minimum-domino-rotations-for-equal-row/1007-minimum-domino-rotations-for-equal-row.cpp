789cb4586363b25c45ecc340d8cf1b5565ebe79f
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-13 18:52:59 +0530

    Time: 284 ms (9.52%), Space: 111.6 MB (84.22%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,37 +1,24 @@
-/* 
-    Time: O(n)
-    Space: O(1)
-    Tag: Arrays
-    Difficulty: M
-*/
-
 class Solution {
 public:
-    int minDominoRotations(vector<int> &tops, vector<int> &bottoms) {
-        int curSwapsTop = 0, curSwapsBot = 0, minSwaps = INT_MAX;
-        for (int i = 1; i <= 6; i++) {
-            curSwapsTop = 0;
-            curSwapsBot = 0;
-            for (int j = 0; j < tops.size(); j++) {
-                if (tops[j] == i && bottoms[j] == i)
-                    continue;
-                else if (tops[j] == i || bottoms[j] == i) {
-                    if (tops[j] == i) {
-                        curSwapsBot++;
-                    }
-                    if (bottoms[j] == i) {
-                        curSwapsTop++;
-                    }
-                } else {
-                    curSwapsTop = INT_MAX;
-                    curSwapsBot = INT_MAX;
-                    break;
-                }
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int toptop=0, topbottom=0, bottomtop=0, bottombottom=0;
+        
+        int n=tops.size();
+        
+        for(int i=0;i<n;i++){
+            if(tops[i]!=tops[0] && bottoms[i]!=tops[0]) toptop=topbottom=-1;
+            if(tops[i]!=bottoms[0] && bottoms[i]!=bottoms[0]) bottomtop=bottombottom=-1;
+            
+            if(toptop!=-1){
+                if(tops[0]==tops[i]) toptop++;
+                if(tops[0]==bottoms[i]) topbottom++;
             }
-            minSwaps = min(minSwaps, min(curSwapsBot, curSwapsTop));
-            // cout << i << " " << minSwaps << endl;
-            // cout << curSwapsBot << " " << curSwapsTop << endl;
+            if(bottombottom!=-1){
+                if(bottoms[0]==tops[i]) bottomtop++;
+                if(bottoms[0]==bottoms[i]) bottombottom++;
+            }
         }
-        return minSwaps == INT_MAX ? -1 : minSwaps;
+        if(toptop==-1 && bottombottom==-1) return -1;
+        return min({n-toptop, n-topbottom, n-bottomtop, n-bottombottom});        
     }
 };
