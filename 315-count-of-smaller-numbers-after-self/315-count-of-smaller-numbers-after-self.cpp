30df86cc710567378cc26be5eada51f34fa4c4c7
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-15 02:18:07 +0530

    Time: 630 ms (57.77%), Space: 211.5 MB (31.68%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,46 +1,53 @@
 class Solution {
+    
     vector<int>res;
     
-    void merge(vector<pair<int,int>>&nums, int start, int end, int mid){
-        // cout<<"start-";
-        int i=start, j=mid+1;
-        vector<pair<int,int>>temp;
-        int count=0;
-        while(i<=mid && j<=end){
-            if(nums[i].first<=nums[j].first) {
-                res[nums[i].second]+=count;
-                temp.push_back(nums[i++]);
+    void merge(vector<pair<int,int>>&nums, int lo, int hi, int mid){
+        
+        int h1=lo, h2=mid+1;
+        
+        vector<pair<int,int>>temp(hi-lo+1);
+        
+        int k=0;
+        
+        while(h1<=mid && h2<=hi){
+            if(nums[h1].first>nums[h2].first){
+                res[nums[h1].second]+=hi-h2+1;
+                temp[k++]=nums[h1++];
+            }else{
+                temp[k++]=nums[h2++];
             }
-            else{
-                count++;
-                temp.push_back(nums[j++]);
-            }
         }
-        while(i<=mid){
-            res[nums[i].second]+=count;
-            temp.push_back(nums[i++]);
+        while(h1<=mid){
+            temp[k++]=nums[h1++];
         }
-        while(j<=end){
-            temp.push_back(nums[j++]);
+        while(h2<=hi){
+            temp[k++]=nums[h2++];
         }
-        j=0;
-        for(i=start;i<=end;i++) nums[i]=temp[j++];
-        // cout<<"end;\n";
+        k=0;
+        for(int i=lo;i<=hi;i++){
+            nums[i]=temp[k++];
+        }
     }
     
-    void divideAndCon(vector<pair<int,int>>&nums, int i, int j){
-        if(i>=j) return;
-        int mid=i+(j-i)/2;
-        divideAndCon(nums,i,mid);
-        divideAndCon(nums,mid+1,j);
-        return merge(nums,i,j,mid);
+    void divideAndMerge(vector<pair<int,int>>&nums, int lo, int hi){
+        if(lo>=hi) return;
+        int mid=lo+(hi-lo)/2;
+        divideAndMerge(nums,lo,mid);
+        divideAndMerge(nums,mid+1,hi);
+        merge(nums,lo,hi,mid);
     }
+    
+    
 public:
     vector<int> countSmaller(vector<int>& nums) {
         res.resize(nums.size(),0);
-        vector<pair<int,int>>temp(nums.size());
-        for(int i=0;i<nums.size();i++) temp[i]={nums[i],i};
-        divideAndCon(temp,0,nums.size()-1);
+        
+        vector<pair<int,int>>t(nums.size());
+        for(int i=0;i<nums.size();i++) t[i]={nums[i],i};
+        
+        divideAndMerge(t,0,nums.size()-1);
         return res;
+        
     }
 };
