4bebb5ed8c43cb0f4a906de2f745da284ae77da8
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-16 02:26:20 +0530

    Time: 215 ms (58.06%), Space: 55 MB (64.08%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,35 +1,41 @@
-/*
-    Time: O(nlogn) // n is no. of Processes
-    Space: O(n)
-    Tag: DP(Knapsack Pattern) with Binary Search
-    Difficulty: M
-    Note: Had to see related topics
-*/
-
 class Solution {
-    vector<int> t;
-    vector<vector<int>>process;
-    vector<int>startTime;
-
-    int solve(int index) {
-        if (index >= startTime.size()) return 0;
-        if (t[index] != -1) return t[index];
-        int res = 0;
-        res = solve(index + 1);
-        int next = lower_bound(startTime.begin(), startTime.end(), process[index][1]) - startTime.begin();
-        res = max(res, process[index][2]+solve(next));
-        return t[index] = res;
-    }
-
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        t.resize(startTime.size() + 1, -1);
-        for (int i = 0; i < startTime.size(); i++)
-            process.push_back({ startTime[i],endTime[i],profit[i] });
-
-        sort(process.begin(), process.end());
-        sort(startTime.begin(), startTime.end());
-        this->startTime=startTime;
-        return solve(0);
+        
+        int n=profit.size();
+        vector<vector<int>>jobs(n);
+        for(int i=0;i<n;i++){
+            jobs[i]={startTime[i],endTime[i],profit[i]};
+        }
+        
+        sort(jobs.begin(),jobs.end(), [](auto& a, auto& b){
+            if(a[1]!=b[1]) return a[1]<b[1];
+            if(a[0]!=b[0]) return a[0]<b[0];
+            return a[2]<b[2];
+        });
+        
+        vector<pair<int,int>>t;
+        
+        for(int i=0;i<n;i++){
+            if(t.empty()) {
+                t.push_back({jobs[i][1],jobs[i][2]});
+                continue;
+            }
+            
+            int startIdx=upper_bound(t.begin(),t.end(),make_pair(jobs[i][0],INT_MAX))-t.begin();
+            int extra=0;
+            if(startIdx!=0) extra=t[startIdx-1].second;
+            // cout<<startIdx<<endl;
+            if(t.back().first==jobs[i][1]) t.back().second=max(t.back().second,jobs[i][2]+extra);
+            else if(t.back().second<jobs[i][2]+extra) t.push_back({jobs[i][1],jobs[i][2]+extra});
+                        
+        }
+        
+        int res=0;
+        for(auto temp:t) {
+            // cout<<temp.first<<" "<<temp.second<<endl;
+            res=max(res,temp.second);
+        }
+        return res;            
     }
 };
