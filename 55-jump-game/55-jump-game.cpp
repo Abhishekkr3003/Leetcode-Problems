11f2e646a952693a7edb980f2e9074c2bda36c3e
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-23 17:46:54 +0530

    Time: 92 ms (59.15%), Space: 48.3 MB (77.60%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -7,7 +7,9 @@ public:
         if(nums.size()==1) return true;
         if(nums[0]==0) return false;
         
-        for(int i=1;i<nums.size();i++){
+        if(maxReach>=nums.size()-1) return true;
+        
+        for(int i=1;i<nums.size()-1;i++){
             maxReach=max(maxReach,i+nums[i]);
             if(maxReach>=nums.size()-1) return true;
             if(i==curReach){
