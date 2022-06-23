018759fceb62fccca902608a273cf875945a5a1f
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-23 18:07:16 +0530

    Time: 64 ms (58.19%), Space: 31.2 MB (85.53%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,21 +1,30 @@
-
-
 class Solution {
-    
 public:
-    bool canReach(vector<int> &arr, int start) {
-        if ((start < 0) || (start >= arr.size()) || (arr[start] == -1)) {
-            // cout << endl;
-            return false;
+    bool canReach(vector<int>& arr, int start) {
+        if(arr[start]==0) return true;
+        queue<int>q;
+        q.push(start);
+        arr[start]=-arr[start];
+        
+        while(!q.empty()){
+            int num=q.front();
+            q.pop();
+            
+            if(num+(arr[num])>=0){
+                if(arr[num+arr[num]]==0) return true;
+                if(arr[num+arr[num]]>0) {
+                    arr[num+arr[num]]*=-1;
+                    q.push(num+arr[num]);
+                }
+            }
+            if(num-(arr[num])<arr.size()){
+                if(arr[num-arr[num]]==0) return true;
+                if(arr[num-arr[num]]>0) {
+                    arr[num-arr[num]]*=-1;
+                    q.push(num-arr[num]);
+                }
+            }
         }
-
-        if (arr[start] == 0) {
-            return true;
-        } else {
-            // cout << "arr[start]:" << arr[start] << endl;
-            int temp = arr[start];
-            arr[start] = -1;
-            return (canReach(arr, start + temp) || canReach(arr, start - temp));
-        }
+        return false;
     }
 };
