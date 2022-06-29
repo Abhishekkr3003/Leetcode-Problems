1e3f7cf3e2c0d367ef370b3c30ad9cc99a45fb50
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-06-30 04:00:50 +0530

    Time: 16 ms (7.54%), Space: 7.8 MB (6.56%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,28 +1,21 @@
-/* 
-    Time: O(n)
-    Space: O(n)
-    Tag: DP (Find the matching opening parentheses and check the length upto that opening parentheses and distance between current closing parentheses and matching opening paranthesis)
-    Difficulty: MH
-*/
-
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int n=s.length();
-        vector<int>t(n+1,0);
         stack<int>st;
+        vector<int>t(s.length()+1,0);
+        t[0]=0;
         int res=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s.length();i++){
             if(s[i]=='(') st.push(i);
-            else{
+            else {
                 if(!st.empty()){
-                    int left=st.top();
+                    t[i+1]=i-st.top()+1+t[st.top()];
                     st.pop();
-                    t[i+1]=t[left]+i-left+1;
+                    res=max(res,t[i+1]);
                 }
             }
-            res=max(res,t[i+1]);
         }
         return res;
+        
     }
 };
