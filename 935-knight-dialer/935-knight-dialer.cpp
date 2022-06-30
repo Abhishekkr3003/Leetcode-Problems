7a3678f3e8762747d8a7b841029990ada14d3830
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-07-01 01:59:34 +0530

    Time: 219 ms (57.61%), Space: 23.5 MB (54.27%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.cpp
+++ 935-knight-dialer/935-knight-dialer.cpp
@@ -1,8 +1,5 @@
 class Solution {
     vector<vector<int>>moves={{4,6},{6,8},{7,9},{4,8},{0,3,9},{},{0,1,7},{2,6},{1,3},{2,4}};
-    
-    int *t[10];
-    
     int dfs(int n, int curNum,vector<vector<int>>&t){
         if(n==0) return 1;
         if(t[curNum][n]!=-1) return t[curNum][n];
