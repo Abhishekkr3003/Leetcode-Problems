a7c53de3ca3446248ab3e1d67b25bc4a57262187
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-07-01 00:59:14 +0530

    Time: 3 ms (68.35%), Space: 7 MB (94.15%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,43 +1,30 @@
-
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-/* 
-    Time: O(nums.size()*log(sum(nums)-min(nums)))
-    Space: O(1)
-    Tag: Binary Search
-    Difficulty: M
-*/
-
 class Solution {
-    bool checkFeas(int m, vector<int> nums, int maxSum) {
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > maxSum) return false;
-            if (nums[i] + sum > maxSum) {
+    
+    bool possible(vector<int>&nums, int m, int sum){
+        int curSum=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>sum) return false;
+            if(curSum+nums[i]>sum){
                 m--;
-                sum = nums[i];
-                continue;
+                curSum=nums[i];
+            }else{
+                curSum+=nums[i];
             }
-            sum += nums[i];
         }
-        return m <= 0 ? false : true;
+        return m>0;
     }
-
+    
 public:
     int splitArray(vector<int>& nums, int m) {
-        int lo = *min_element(nums.begin(), nums.end()), hi = accumulate(nums.begin(), nums.end(), 0);
-        int ans, mid;
-
-        while (lo <= hi) {
-            mid = lo + (hi - lo) / 2;
-            if (checkFeas(m, nums, mid)) {
-                ans = mid;
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
+        int lo=*min_element(nums.begin(),nums.end()), hi=accumulate(nums.begin(),nums.end(),0);
+        int mid=0, ans=-1;
+        
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(possible(nums,m,mid)){
+                ans=mid;
+                hi=mid-1;
+            }else lo=mid+1;
         }
         return ans;
     }
