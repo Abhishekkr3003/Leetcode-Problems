7cf494530c2d9ede970490d226792e54c16ab0ec
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-07-04 17:42:36 +0530

    Time: 573 ms (5.29%), Space: 129.6 MB (24.50%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,55 +1,29 @@
-/* 
-    Time: O(n)
-    Space: O(n)
-    Tag: DP (Knapsack Pattern)
-    Difficulty: H
-*/
-
 class Solution {
-    vector<vector<int>> t;
-    vector<int> nums1, nums2;
-
-    int findMinSwaps(int index, bool isSwaped) {
-        // cout<<index<<endl;
-        if (index == nums1.size()) return 0;
-        int res = INT_MAX;
-        if (nums1[index] > nums2[index - 1] && nums1[index - 1] < nums2[index]) {
-            if (t[index][0] != -1)
-                res = min(res, t[index][0]);
-            else {
-                swap(nums1[index], nums2[index]);
-                int temp = findMinSwaps(index + 1, true);
-                if (temp != INT_MAX)
-                    res = min(res, 1 + temp);
-                swap(nums1[index], nums2[index]);
-                t[index][0] = 1 + temp;
-            }
+    vector<vector<int>>t;
+    
+    int solve(vector<int>&nums1, vector<int>&nums2, int idx, bool swapped){
+        if(idx==nums1.size()) return 0;
+        if(t[idx][swapped]!=-1) return t[idx][swapped];
+        
+        int res=INT_MAX-1;
+        
+        if(nums1[idx]>nums1[idx-1] && nums2[idx]>nums2[idx-1]){
+            res=min(res,solve(nums1,nums2,idx+1,false));
         }
-        if (nums1[index] > nums1[index - 1] && nums2[index] > nums2[index - 1]) {
-            if (t[index][1] != -1)
-                res = min(res, t[index][1]);
-            else {
-                int temp = findMinSwaps(index + 1, false);
-                if (temp != INT_MAX)
-                    res = min(res, temp);
-                t[index][1] = temp;
-            }
+        if(nums1[idx]>nums2[idx-1] && nums2[idx]>nums1[idx-1]){
+            swap(nums1[idx],nums2[idx]);
+            res=min(res,1+solve(nums1,nums2,idx+1,true));
+            swap(nums1[idx],nums2[idx]);
         }
-        return res;
+        return t[idx][swapped]=res;
     }
-
 public:
-    int minSwap(vector<int> &nums1, vector<int> &nums2) {
-        t.resize(nums1.size()+1, vector<int>(2, -1));
-        this->nums1 = nums1;
-        this->nums2 = nums2;
-        int res = findMinSwaps(1, false);
-        // cout<<res<<endl;
-        swap(this->nums1[0], this->nums2[0]);
-        int temp = findMinSwaps(1, true);
-        if (temp != INT_MAX)
-            res = min(res, 1 + temp);
-        swap(this->nums1[0], this->nums2[0]);
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        t.resize(nums1.size(), vector<int>(2,-1));
+        int res=INT_MAX-1;
+        res=min(res,solve(nums1,nums2,1,false));
+        swap(nums1[0],nums2[0]);
+        res=min(res,1+solve(nums1,nums2,1,true));
         return res;
     }
 };
