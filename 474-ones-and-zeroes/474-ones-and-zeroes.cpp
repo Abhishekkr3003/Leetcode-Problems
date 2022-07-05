5ff4e4d47b3f6564a9af3047891cae9fdb10c50d
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-07-05 17:13:36 +0530

    Time: 295 ms (73.86%), Space: 89 MB (21.42%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,22 +1,35 @@
 class Solution {
+    vector<vector<vector<int>>>t;
+    
+    int solve(vector<pair<int,int>>& strs, int m, int n, int idx){
+        if(idx==strs.size()) return 0;
+        
+        if(t[m][n][idx]!=-1) return t[m][n][idx];
+        
+        int res=solve(strs,m,n,idx+1);
+        
+        if(m>=strs[idx].first && n>=strs[idx].second){
+            res=max(res,1+solve(strs,m-strs[idx].first, n-strs[idx].second,idx+1));
+        }
+        
+        return t[m][n][idx]=res;
+        
+    }
+    
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        int res=0;
-        vector<vector<int>>t(m+1,vector<int>(n+1,0));
-        for(string &str:strs){
-            int zeros=0;
-            int ones=0;
-            for(int i=0;i<str.length();i++){
-                if(str[i]=='0') zeros++;
+        t.resize(m+1,vector<vector<int>>(n+1, vector<int>(strs.size(),-1)));
+        
+        vector<pair<int,int>>temp(strs.size());
+        
+        for(int i=0;i<strs.size();i++){
+            int freq=0;
+            for(char ch:strs[i]){
+                if(ch=='0') freq++;
             }
-            ones=str.length()-zeros;
-            for(int i=m;i>=zeros;i--){
-                for(int j=n;j>=ones;j--){
-                    t[i][j]=max(t[i][j],1+t[i-zeros][j-ones]);
-                    res=max(res,t[i][j]);
-                }
-            }
+            temp[i]={freq, strs[i].length()-freq};
         }
-        return res;
+        // for(auto p:temp) cout<<p.first<<" "<<p.second<<endl;
+        return solve(temp,m,n,0);
     }
 };
