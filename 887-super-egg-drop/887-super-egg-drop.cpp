d024c8623e387ff7fbd4797319bf342783e06dea
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-07-07 00:12:13 +0530

    Time: 359 ms (14.48%), Space: 47.4 MB (8.16%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,37 +1,29 @@
 class Solution {
-    vector<vector<int>> t;
-    int n;
-    int solve(int f, int e) {
-        if (f == 0) return 0;
-        if (e == 1) return f;
-        if (t[f][e] != -1) return t[f][e];
-        int res = n + 1;
-        int lo=1, hi=f;
-        int mid=0, t1=0, t2=0;
+    vector<vector<int>>t;
+    
+    int solve(int n, int k){
+        if(n==0) return 0;
+        if(k==1) return n;
+        if(t[n][k]!=-1) return t[n][k];
+        
+        int res=INT_MAX;
+        int lo=1, hi=n;
+        int mid=0;
         while(lo<=hi){
             mid=lo+(hi-lo)/2;
-            t1=solve(mid-1,e-1);
-            t2=solve(f-mid,e);
-            if(t1<t2){
-                lo=mid+1;
-            }else if(t2<t1){
-                hi=mid-1;
-            }else{
-                res=min(res,1+max(t1,t2));   
-                break;
-            }
-            res=min(res,1+max(t1,t2));     
+            int t1=solve(mid-1,k-1);
+            int t2=solve(n-mid,k);
+            
+            res=min(res,1+max(t1,t2));
+            if(t1>t2) hi=mid-1;
+            else lo=mid+1;
         }
-        // for (int k = 1; k <= f; k++) {
-        //     res = min(res, 1 + max(solve(k - 1, e - 1), solve(f - k, e)));
-        // }
-        return t[f][e] = res;
+        return t[n][k]=res;
     }
-
+    
 public:
     int superEggDrop(int k, int n) {
-        t.resize(n + 1, vector<int>(k + 1, -1));
-        this->n = n;
-        return solve(n, k);
+        t.resize(n+1, vector<int>(k+1,-1));
+        return solve(n,k);
     }
 };
