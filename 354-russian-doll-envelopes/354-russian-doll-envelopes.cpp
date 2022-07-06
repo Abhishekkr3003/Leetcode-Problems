485a418583dafc843abbb6d144fd93c06f455d0a
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-07-07 01:28:36 +0530

    Time: 735 ms (15.97%), Space: 77.6 MB (28.98%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,18 +1,19 @@
-// H (To find the logic that sort width dec and height inc) | DP (LIS) | T: O(nlogn) | S: O(n)
-
 class Solution {
 public:
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-        sort(envelopes.begin(),envelopes.end(), [](vector<int>&a, vector<int>&b){
-            if(a[1]==b[1]) return a[0]>b[0];
-            return a[1]<b[1];
+    int maxEnvelopes(vector<vector<int>>& e) {
+        sort(e.begin(),e.end(),[](vector<int>&a, vector<int>&b){
+            if(a[0]==b[0]) return a[1]>b[1];
+            return a[0]<b[0];
         });
-        vector<int>st;
-        for(int i=0;i<envelopes.size();i++){
-            auto it=lower_bound(st.begin(),st.end(),envelopes[i][0]);
-            if(it==st.end()) st.push_back(envelopes[i][0]);
-            else *it=envelopes[i][0];
+        vector<int>t;
+        
+        for(int i=0;i<e.size();i++){
+            auto it=lower_bound(t.begin(),t.end(),e[i][1]);
+            if(it==t.end()) t.push_back(e[i][1]);
+            else *it=e[i][1];
         }
-        return st.size();
+        
+        return t.size();
+        
     }
 };
