18fa636b905eca7f118697d38c066d876b53289d
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-07-12 23:18:22 +0530

    Time: 601 ms (37.52%), Space: 10.2 MB (30.06%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,20 +1,25 @@
 class Solution {
+    // set<vector<int>>mp;
     
-    bool canWe(vector<int>&ms, int idx, int curSum, int k, int &reqSum){
-        if(curSum>reqSum) return false;
-        if(idx==ms.size()){
-            if(k==3) return (curSum==reqSum);
-            else if(curSum==reqSum) return canWe(ms,0,0,k+1,reqSum);
-            return false;
-        }
+    bool canWe(vector<int>&ms, int idx, int sum1, int sum2, int sum3, int sum4, int &reqSum){
         
-        if(canWe(ms,idx+1,curSum,k,reqSum)) return true;
-        if(ms[idx]!=-1){
-            int oldNum=ms[idx];
-            ms[idx]=-1;
-            if(canWe(ms,idx+1,curSum+oldNum,k,reqSum)) return true;
-            ms[idx]=oldNum;
+        // cout<<idx<<" "<<sum1<<" "<<sum2<<" "<<sum3<<endl;
+        if(sum1>reqSum || sum2>reqSum || sum3>reqSum || sum4>reqSum) return false;
+        
+        if(idx>=ms.size()){
+            return (sum1==reqSum && sum2==reqSum && sum3==reqSum && sum4==reqSum);
         }
+         
+        // vector<int> key={sum1,sum2,sum3,sum4};
+        
+        // if(mp.find(key)!=mp.end()) return false;
+        
+        if(canWe(ms,idx+1,sum1+ms[idx],sum2,sum3,sum4,reqSum)) return true;
+        if(canWe(ms,idx+1,sum1,sum2+ms[idx],sum3,sum4,reqSum)) return true;
+        if(canWe(ms,idx+1,sum1,sum2,sum3+ms[idx],sum4,reqSum)) return true;
+        if(canWe(ms,idx+1,sum1,sum2,sum3,sum4+ms[idx],reqSum)) return true;
+        
+        // mp.insert(key);
         return false;
     }
     
@@ -23,7 +28,7 @@ public:
         int sum=accumulate(ms.begin(),ms.end(),0);
         if(sum%4!=0) return false;
         sum/=4;
-        
-        return canWe(ms,0,0,1,sum);
+        sort(ms.rbegin(),ms.rend());
+        return canWe(ms,0,0,0,0,0,sum);
     }
 };
