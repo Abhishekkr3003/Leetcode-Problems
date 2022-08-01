7481ecf9dcc1ee36c4bd6c5d9b70f8e0614231f9
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-08-01 11:13:02 +0530

    Time: 285 ms (56.35%), Space: 12.5 MB (52.98%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -4,21 +4,30 @@ public:
         int sum=accumulate(nums.begin(),nums.end(),0);
         if(sum&1) return false;
         sum/=2;
-        
-        vector<bool>t(sum+1,false), nxt(sum+1);
-        t[0]=true;
-        
-        for(int i=0;i<nums.size();i++){
-            nxt[0]=true;
-            for(int j=1;j<=sum;j++){
-                if(nums[i]<=j){
-                    nxt[j]=t[j]||t[j-nums[i]];
-                }else{
-                    nxt[j]=t[j];
+        int n=nums.size();
+        vector<vector<bool>> t(n + 1, vector<bool>(sum + 1, false));
+
+        for (int i = 0; i <= n; i++) {
+            t[i][0] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (j >= nums[i - 1]) {
+                    t[i][j] = t[i - 1][j] || t[i - 1][j - nums[i - 1]];
+                } else {
+                    t[i][j] = t[i - 1][j];
                 }
             }
-            t=nxt;
         }
-        return t[sum];
+
+//         for (int i = 0; i <= n; i++) {
+//             for (int j = 0; j <= sum; j++) {
+//                 cout << t[i][j] << " ";
+//             }
+//             cout << endl;
+//         }
+
+        return t[n][sum];
     }
 };
