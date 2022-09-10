011468dc09171d4aa75433ce9c8f3d39b4598333
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-09-11 00:28:34 +0530

    Time: 24 ms (41.12%), Space: 10.9 MB (84.96%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,9 +3,7 @@ public:
     int maxProfit(int k, vector<int>& prices) {        
         int n=prices.size();
         if(n==0) return 0;
-        
         vector<int>t(n,0), temp(n);
-        
         for(int i=0;i<k;i++){
             temp[0]=0;
             int bp=-prices[0];
