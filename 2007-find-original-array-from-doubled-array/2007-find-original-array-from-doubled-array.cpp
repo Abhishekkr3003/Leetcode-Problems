6b9067ad8a88ce571e97c4196686eb7ee193029d
Abhishek Kumar <abhishekkr3003@gmail.com>
2022-09-16 13:50:57 +0530

    Time: 823 ms (31.74%), Space: 144.2 MB (51.94%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,30 +1,26 @@
-/* 
-    Time: O()
-    Space: O()
-    Tag: 
-    Difficulty: 
-*/
-
 class Solution {
 public:
-    vector<int> findOriginalArray(vector<int> &changed) {
-        map<int, int> mp;
-        for (int &num : changed) {
-            mp[num]++;
-        }
-        vector<int> res;
-        for (auto &p : mp) {
-            if (p.first == 0) {
-                if (p.second & 1)
-                    return {};
-                else {
-                    for (int i = 0; i < p.second / 2; i++) res.push_back(0);
-                }
-            } else if (p.second > 0) {
-                mp[p.first * 2] -= p.second;
-                if (mp[p.first * 2] < 0) return {};
-                for (int i = 0; i < p.second; i++) res.push_back(p.first);
-            }
+    vector<int> findOriginalArray(vector<int>& changed) {
+        map<int,int>mp;
+        for(int i=0;i<changed.size();i++) mp[changed[i]]++;
+        
+        vector<int>res;
+        
+        for(auto p:mp){
+           if(p.first==0){
+               if(p.second&1) return {};
+               else{
+                   for(int i=0;i<p.second/2;i++) res.push_back(0);
+               }
+           }
+            
+           else if(p.second>0){
+               mp[2*p.first]-=p.second;
+               if(mp[2*p.first]<0) return {};
+               else {
+                   for(int i=0;i<p.second;i++) res.push_back(p.first);
+               }
+           } 
         }
         return res;
     }
